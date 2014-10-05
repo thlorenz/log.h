347767cbc75c5a17fabb3257fa1a2ba0f2dfa448
Thorsten Lorenz <thlorenz@gmx.de>
2014-10-05 16:09:24 -0400

    error, warn - nocolors bug fix

--- log.h
+++ log.h
@@ -22,8 +22,8 @@
 #define clean_errno() (errno == 0 ? "None" : strerror(errno))
 
 #ifdef LOG_NOCOLORS
-  #define log_error(M, ...) fprintf(stderr,  "ERR   " M " at %s (%s:%d) errno:\n", ##__VA_ARGS__, __func__, __FILE__, __LINE__, clean_errno())
-  #define log_warn(M, ...) fprintf(stderr, "WARN  " M " at %s (%s:%d) errno:\n", ##__VA_ARGS__, __func__, __FILE__, __LINE__, clean_errno())
+  #define log_error(M, ...) fprintf(stderr,  "ERR   " M " at %s (%s:%d) errno:%s\n", ##__VA_ARGS__, __func__, __FILE__, __LINE__, clean_errno())
+  #define log_warn(M, ...) fprintf(stderr, "WARN  " M " at %s (%s:%d) errno:%s\n", ##__VA_ARGS__, __func__, __FILE__, __LINE__, clean_errno())
   #define log_info(M, ...) fprintf(stderr, "INFO  " M " at %s (%s:%d)\n", ##__VA_ARGS__, __func__, __FILENAME__, __LINE__)
 #else
   #define log_error(M, ...) fprintf(stderr,  "\33[31mERR\33[39m   " M "  \33[90m at %s (%s:%d) \33[94merrno: %s\33[39m\n", ##__VA_ARGS__, __func__, __FILE__, __LINE__, clean_errno())
